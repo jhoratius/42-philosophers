543e8555c994222132fac6c9e11183f10ee83f58
jhoratius <julien.horatius@gmail.com>
2024-07-16 18:40:39 +0200

    experienced to create a thread that works, even two and giving arguments to threads

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jhoratiu <jhoratiu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 18:30:02 by jhoratiu          #+#    #+#             */
-/*   Updated: 2024/07/15 19:50:32 by jhoratiu         ###   ########.fr       */
+/*   Updated: 2024/07/16 18:38:51 by jhoratiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,54 @@
 
 int	main(int ac, char** av, char **env)
 {
-	int			i;
-	pthread_t	thread;
-	// void		*ft_routine(void *);
+	pthread_t		thread1;
+	pthread_t		thread2;
+	t_meal_table	table;
+	void			*ft_routine(void *add);
 
-	i = 2;
+	int				value1 = 1;
+	int				value2 = 2;
 	// check if there is an exact number of arguments
-	if (ac != 6)
-		return (1);
+	//if (ac != 6)
+	//	return (1);
 	// check if all the arguments are numbers
-	if (parsing(av) == false)
+	//if (!parsing(av))
+	//	return (1);
+	// add infos in struct table and free if return value is NULL
+	if (append_infos_table(&table, av) == NULL)
 		return (1);
+
 	// creates a thread
-	pthread_create(&thread, NULL, ft_routine, NULL);
+	pthread_create(&thread1, NULL, ft_routine, (void *) &value1);
+	pthread_create(&thread2, NULL, ft_routine, (void *) &value2);
+	
 	// 1st arg is the id/ptr
 	// 2nd arg is attributes (mostly NULL)
 	// 3rd arg is the function the thread starts to execute
 	// 4th arg is arguments for the routine, can be a structure for multiples args
+
+	//ft_routine((void *) &value1);
+	//ft_routine((void *) &value2);
+
+	// join the thread
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
 	
 	// quit a thread (must be inside the pthread_create function passed in argument)
 	// pthread_exit();
 	return (0);
 }
 
-void	*ft_routine(void *ptr)
+void	*ft_routine(void *add)
 {
+	int		*add_num = (int *) (add);
+	printf("start thread ");
+	printf("%d\n", *add_num);
+	// pthread_exit(ptr);
 	return (NULL);
+}
+
+bool	ft_append_infos_table(t_meal_table *table, char **av)
+{
+	return (true);
 }
--- src/parsing.c
+++ src/parsing.c
@@ -6,14 +6,23 @@
 /*   By: jhoratiu <jhoratiu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 18:34:50 by jhoratiu          #+#    #+#             */
-/*   Updated: 2024/07/15 19:25:48 by jhoratiu         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:26:24 by jhoratiu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-bool	parsing(char **av)
+int	*parsing(char **av)
 {
+	int	i;
+	int	tab[ft_count_av(av)];
+
+	i = 1;
+	while (av[i] != NULL)
+	{
+		if(ft_atoi2(av[i], tab[i]) == false)
+			return (NULL);
+	}
 	// check first argument (av[1]) so that it's a number
 	// [number of philosophers]
 
@@ -28,4 +37,15 @@ bool	parsing(char **av)
 
 	// check the fifth and last argument (av[5]), so that it's a number
 	// [number of time each philosopher must eat]
+	return (tab);
+}
+
+int	ft_count_av(char **av)
+{
+	int	i;
+
+	i = 0;
+	while (av[i] != NULL)
+		i++;
+	return (i);
 }
